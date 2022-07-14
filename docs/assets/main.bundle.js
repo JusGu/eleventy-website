df9463102827eb90914f598b6c90afa05b44fe24
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-07-13 21:43:12 -0400

    Fixed CSS on justingu.tech part 2

--- docs/assets/main.bundle.js
+++ docs/assets/main.bundle.js
@@ -1,1 +1,38 @@
-!function(){"use strict";const e=e=>"language"===e.className.substring(0,8);(()=>{const t=document.getElementsByTagName("pre");if(!t)return;for(const n of t)e(n)&&(n.innerHTML=`<div class="copy">copy</div>${n.innerHTML}`);new ClipboardJS(".copy",{target:e=>e.nextElementSibling}).on("success",(e=>{e.trigger.textContent="copied!",setTimeout((()=>{e.clearSelection(),e.trigger.textContent="copy"}),2e3)}))})()}();
+(function () {
+  'use strict';
+
+  // Adapted from https://codepen.io/wilbo/pen/xRVLOj by Wilbert Schepenaar.
+
+  const CLEAR_SELECTION_DELAY = 2000;
+
+  const isPrismClass = preTag =>
+    preTag.className.substring(0, 8) === 'language';
+
+  const handleCodeCopying = () => {
+    const preTags = document.getElementsByTagName('pre');
+
+    if (!preTags) return;
+
+    for (const preTag of preTags)
+      if (isPrismClass(preTag))
+        preTag.innerHTML = `<div class="copy">copy</div>${preTag.innerHTML}`;
+
+    const clipboard = new ClipboardJS('.copy', {
+      target: trigger => trigger.nextElementSibling,
+    });
+
+    clipboard.on('success', event => {
+      event.trigger.textContent = 'copied!';
+
+      setTimeout(() => {
+        event.clearSelection();
+        event.trigger.textContent = 'copy';
+      }, CLEAR_SELECTION_DELAY);
+    });
+  };
+
+  console.log('Dev mode is currently enabled.');
+
+  handleCodeCopying();
+
+})();
